8d6db37eae54daaa665b209f122609464db06681
th3hybrid <emmanueloludare928@gmail.com>
2024-12-02 16:20:21 +0100

    implemented minimalist account abstraction

---
+++ src/ethereum/MinimalAccount.sol
@@ -0,0 +1,44 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import {IAccount} from "lib/account-abstraction/contracts/interfaces/IAccount.sol";
+import {PackedUserOperation} from "lib/account-abstraction/contracts/interfaces/PackedUserOperation.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {MessageHashUtils} from "@openzeppelin/contracts/utils/cryptography/MessageHashUtils.sol";
+import {ECDSA} from "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+import {SIG_VALIDATION_FAILED,SIG_VALIDATION_SUCCESS} from "lib/accoount-abstraction/contracts/core/Helpers.sol";
+
+
+contract MinimalAccount is IAccount,Ownable {
+    constructor() Ownable(msg.sender) {
+
+    }
+
+    //a signature is valid if its the MinimalAccount owner 
+    function validateUserOp(
+        PackedUserOperation calldata userOp,
+        bytes32 userOpHash,
+        uint256 missingAccountFunds
+    ) external returns (uint256 validationData){
+        validationData = _validateSignature(userOp,userOpHash);
+        //might decide to validate nonce
+        _payPrefund(missingAccountFunds);
+        return validationData;
+    }
+
+    //userOpHash is the EIP-191 version of the signed hash
+    function _validateSignature(PackedUserOperation calldata userOp,bytes32 userOpHash) internal view returns (uint256 validationData) {
+        bytes32 ethSignedMessageHash = MessageHashUtils.toEthSignedMessageHash(userOpHash);
+        address signer = ECDSA.recover(ethSignedMessageHash,userOp.signature);
+        if(signer != owner()) {
+            return SIG_VALIDATION_FAILED;
+        }
+        return SIG_VALIDATION_SUCCESS;
+    }
+
+    function _payPrefund(uint256 missingAccountFunds) internal {
+        if(missingAccountFunds != 0) {
+            (bool success,) = payable(msg.sender).call{value:missingAccountFunds,gas:type(uint256).max}("");
+        }
+    }
+}

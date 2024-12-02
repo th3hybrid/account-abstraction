29c38c2e943e4bdc245d9424fb126f03616ba05b
th3hybrid <emmanueloludare928@gmail.com>
2024-12-02 21:18:01 +0100

    added functionality to the minimal account

--- src/ethereum/MinimalAccount.sol
+++ src/ethereum/MinimalAccount.sol
@@ -6,39 +6,92 @@ import {PackedUserOperation} from "lib/account-abstraction/contracts/interfaces/
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {MessageHashUtils} from "@openzeppelin/contracts/utils/cryptography/MessageHashUtils.sol";
 import {ECDSA} from "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
-import {SIG_VALIDATION_FAILED,SIG_VALIDATION_SUCCESS} from "lib/accoount-abstraction/contracts/core/Helpers.sol";
+import {SIG_VALIDATION_FAILED, SIG_VALIDATION_SUCCESS} from "lib/account-abstraction/contracts/core/Helpers.sol";
+import {IEntryPoint} from "lib/account-abstraction/contracts/interfaces/IEntryPoint.sol";
 
+contract MinimalAccount is IAccount, Ownable {
+    //errors
+    error MinimalAccount__NotFromEntryPoint();
+    error MinimalAccount__NotFromEntryPointOrOwner();
+    error MinimalAccount__CallFailed(bytes);
+    address private immutable i_entryPoint;
 
-contract MinimalAccount is IAccount,Ownable {
-    constructor() Ownable(msg.sender) {
-
+    constructor(address entryPoint) Ownable(msg.sender) {
+        i_entryPoint = entryPoint;
     }
 
-    //a signature is valid if its the MinimalAccount owner 
+    modifier requireFromEntryPoint() {
+        if (msg.sender != address(i_entryPoint)) {
+            revert MinimalAccount__NotFromEntryPoint();
+        }
+        _;
+    }
+
+    modifier requireFromEntryPointOrOwner() {
+        if (msg.sender != address(i_entryPoint) && msg.sender != owner()) {
+            revert MinimalAccount__NotFromEntryPointOrOwner();
+        }
+        _;
+    }
+
+    receive() external payable() {
+        
+    }
+
+    //external functions
+    function execute(
+        address dest,
+        uint256 value,
+        bytes calldata functionData
+    ) external payable requireFromEntryPoint {
+        (bool success, bytes memory result) = dest.call{value: value}(
+            functionData
+        );
+        if (!success) {
+            revert MinimalAccount__CallFailed(result);
+        }
+    }
+
+    //a signature is valid if its the MinimalAccount owner
     function validateUserOp(
         PackedUserOperation calldata userOp,
         bytes32 userOpHash,
         uint256 missingAccountFunds
-    ) external returns (uint256 validationData){
-        validationData = _validateSignature(userOp,userOpHash);
+    ) external requireFromEntryPoint returns (uint256 validationData) {
+        validationData = _validateSignature(userOp, userOpHash);
         //might decide to validate nonce
         _payPrefund(missingAccountFunds);
         return validationData;
     }
 
+    //internal functions
     //userOpHash is the EIP-191 version of the signed hash
-    function _validateSignature(PackedUserOperation calldata userOp,bytes32 userOpHash) internal view returns (uint256 validationData) {
-        bytes32 ethSignedMessageHash = MessageHashUtils.toEthSignedMessageHash(userOpHash);
-        address signer = ECDSA.recover(ethSignedMessageHash,userOp.signature);
-        if(signer != owner()) {
+    function _validateSignature(
+        PackedUserOperation calldata userOp,
+        bytes32 userOpHash
+    ) internal view returns (uint256 validationData) {
+        bytes32 ethSignedMessageHash = MessageHashUtils.toEthSignedMessageHash(
+            userOpHash
+        );
+        address signer = ECDSA.recover(ethSignedMessageHash, userOp.signature);
+        if (signer != owner()) {
             return SIG_VALIDATION_FAILED;
         }
         return SIG_VALIDATION_SUCCESS;
     }
 
     function _payPrefund(uint256 missingAccountFunds) internal {
-        if(missingAccountFunds != 0) {
-            (bool success,) = payable(msg.sender).call{value:missingAccountFunds,gas:type(uint256).max}("");
+        if (missingAccountFunds != 0) {
+            (bool success, ) = payable(msg.sender).call{
+                value: missingAccountFunds,
+                gas: type(uint256).max
+            }("");
+            (success);
         }
+    }
+
+    //getters
+    function getEntryPoint() external view returns (address) {
+        return address(i_entryPoint);
     }
 }

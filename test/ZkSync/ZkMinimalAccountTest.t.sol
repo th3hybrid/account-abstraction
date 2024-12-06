9db7c046db437247c2589376076e3e4fb5602e51
th3hybrid <emmanueloludare928@gmail.com>
2024-12-06 15:52:59 +0100

    in zksync tests

---
+++ test/ZkSync/ZkMinimalAccountTest.t.sol
@@ -0,0 +1,59 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import {Test} from "forge-std/Test.sol";
+import {ZkMinimalAccount} from "src/zksync/ZkMinimalAccount.sol";
+import {ERC20Mock} from "@openzeppelin/contracts/mocks/token/ERC20Mock.sol";
+import {Transaction, MemoryTransactionHelper} from "lib/foundry-era-contracts/src/system-contracts/contracts/libraries/MemoryTransactionHelper.sol";
+
+contract ZkMinimalAccountTest is Test {
+    ZkMinimalAccount minimalAccount;
+    ERC20Mock usdc;
+    uint256 constant AMOUNT = 1e18;
+
+    function setUp() public {
+        minimalAccount = new ZkMinimalAccount();
+        usdc = new ERC20Mock();
+    }
+
+    //Arrange
+    //Act
+    //Assert
+
+    function testZkOwnerCanExecuteCommands() public {
+        //Arrange
+        address dest = address(usdc);
+        uint256 value = 0;
+        bytes memory functionData = abi.encodeWithSelector(ERC20Mock.mint.selector,address(minimalAccount),AMOUNT);
+        Transaction memory transaction = _createUnsignedTransaction(minimalAccount.owner(),113,dest,value,functionData);
+        //Act
+        vm.prank(minimalAccount.owner());
+        minimalAccount.executeTransaction(bytes32(0),bytes32(0),transaction);
+        //Assert
+        assertEq(usdc.balanceOf(address(minimalAccount)),AMOUNT);
+    }
+
+    //helpers
+    function _createUnsignedTransaction(address from,uint8 transactionType,uint256 value,bytes memory data) internal view returns(Transaction memory) {
+        uint256 nonce = vm.getNonce(address(minimalAccount));
+        bytes32 [] memory factoryDeps = new bytes32[](0);
+        return Transaction({
+            txType:transactionType, //type 113 (0x71).
+            from : uint256(uint160(from)),
+            to: uint256(uint160(to)),
+            gasLimit: 16777216,
+            gasPerPubdataByteLimit:16777216,
+            maxFeePerGas:16777216,
+            maxPriorityFeePerGas:16777216,
+            paymaster:0,
+            nonce: nonce,
+            value:value,
+            reserved:[uint256(0),uint256(0),uint256(0),uint256(0)],
+            data:data,
+            signature:hex""",
+            factoryDeps:factoryDeps,
+            paymasterInput:hex"",
+            reservedDynamic: hex""
+        })
+    }
+}
